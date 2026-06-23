42335e93c32a315a0bed1e13b58a4de645809bc2
bhaskar-hhp <bhaskar-hhp@users.noreply.github.com>
2026-06-23 17:33:45 +0000

    Add WebViewClient to prevent external browser

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.webviewapp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         WebView wv = new WebView(this);
+        wv.setWebViewClient(new WebViewClient());
         wv.getSettings().setJavaScriptEnabled(true);
         wv.loadUrl("http://161.118.189.168:8082/");
         setContentView(wv);
